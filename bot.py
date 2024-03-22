33417e6f0c0408a66312a1fe613cfbeae46ed877
Ravachol <ravachol.yang@gmail.com>
2024-03-22 23:02:44 +0800

    add /random_mono command to generate monospace text for directly copy

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from config import config
 # handlers
 from app.handlers.info import get_info
 from app.handlers.random_text import get_random_text
+from app.handlers.random_text import get_random_text_mono
 from app.handlers.inline import inline_dispatch
 
 # pyTelegramBotAPI
@@ -16,7 +17,8 @@ bot = telebot.TeleBot(config.BOT_TOKEN)
 
 def handlers():
     bot.register_message_handler(get_info, commands=['start', 'help'], pass_bot=True)
-    bot.register_message_handler(get_random_text, commands=['random_text', 'random'], pass_bot=True)
+    bot.register_message_handler(get_random_text, commands=['random'], pass_bot=True)
+    bot.register_message_handler(get_random_text_mono, commands=['random_mono'], pass_bot=True)
     bot.register_inline_handler(inline_dispatch, lambda query: query, pass_bot=True)
     
 handlers()
