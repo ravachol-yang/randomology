1081d5af71d758ebc30242b394fc907d9bcd82e3
Ravachol <ravachol.yang@gmail.com>
2024-03-27 13:40:09 +0800

    fixed typo

--- configs/dirs.py
+++ configs/dirs.py
@@ -8,4 +8,4 @@ AUDIO_DIR = os.path.join(STORAGE_DIR, "audio")
 
 PUBLIC_DIR = os.path.join(ROOT_DIR, "public")
 PUBLIC_STORAGE_DIR = os.path.join(PUBLIC_DIR, "storage")
-PUBLIC_AUDIO_DIR = os.path.join(PUBLIC_STORAGE_DIR. "audio")
+PUBLIC_AUDIO_DIR = os.path.join(PUBLIC_STORAGE_DIR, "audio")
