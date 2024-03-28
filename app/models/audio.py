6a10485b71f1b9b586ead6024b078ff82ba48333
Ravachol <ravachol.yang@gmail.com>
2024-03-28 19:13:01 +0800

    remove https

--- app/models/audio.py
+++ app/models/audio.py
@@ -19,7 +19,7 @@ class Audio(Base):
 
     MSG_TYPE:str = MsgType.AUDIO
 
-    HOST_PREFIX = "https://{}:{}".format(env.WEBHOOK_HOST, env.WEBHOOK_PORT)
+    HOST_PREFIX = "http://{}:{}".format(env.WEBHOOK_HOST, env.WEBHOOK_PORT)
     
     SAMPLE_RATE = 8000
     FREQ_HIGH = 800
