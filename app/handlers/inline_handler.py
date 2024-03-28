41c60130217916371380de9b0986714206917cee
Ravachol <ravachol.yang@gmail.com>
2024-03-28 19:51:53 +0800

    fix url

--- app/handlers/inline_handler.py
+++ app/handlers/inline_handler.py
@@ -9,12 +9,12 @@ from app.models.voice import Voice
 def inline_dispatch(inline_query, bot: TeleBot, data:dict):
     options = data['options']
     text = Text()
-    voice = Voice().generate(options).content()
+    voice = Voice()
     try:
         text = types.InlineQueryResultArticle('1', 'Random Text',
                                               types.InputTextMessageContent(text.generate(options).content()))
         voice = types.InlineQueryResultVoice('2', title = 'Random Voice',
-                                             voice_url=voice)
+                                             voice_url=voice.generate(options).content())
         bot.answer_inline_query(inline_query.id, [text, voice], cache_time=0)
     except Exception as e:
         print(e)
