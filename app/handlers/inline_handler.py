9a74bdd04686d97aef9676b325982b421f202a81
Ravachol <ravachol.yang@gmail.com>
2024-03-28 19:50:49 +0800

    fix title

--- app/handlers/inline_handler.py
+++ app/handlers/inline_handler.py
@@ -13,7 +13,7 @@ def inline_dispatch(inline_query, bot: TeleBot, data:dict):
     try:
         text = types.InlineQueryResultArticle('1', 'Random Text',
                                               types.InputTextMessageContent(text.generate(options).content()))
-        voice = types.InlineQueryResultVoice('2', caption = 'Random Voice',
+        voice = types.InlineQueryResultVoice('2', title = 'Random Voice',
                                              voice_url=voice)
         bot.answer_inline_query(inline_query.id, [text, voice], cache_time=0)
     except Exception as e:
