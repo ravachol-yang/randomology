fa461db9e1c1bad2a0e43e7d37b1ef360307c147
Ravachol <ravachol.yang@gmail.com>
2024-03-28 18:43:36 +0800

    add debug point

--- app/models/audio.py
+++ app/models/audio.py
@@ -18,7 +18,7 @@ class Audio(Base):
 
     MSG_TYPE:str = MsgType.AUDIO
 
-    HOST_PREFIX = "https://{}:{}/".format(env.WEBHOOK_HOST, env.WEBHOOK_PORT)
+    HOST_PREFIX = "https://{}:{}".format(env.WEBHOOK_HOST, env.WEBHOOK_PORT)
     
     SAMPLE_RATE = 8000
     FREQ_HIGH = 800
@@ -116,6 +116,7 @@ class Audio(Base):
 
         audio.writeframes(values_str)
         audio.close()
+        print(self._content)
 
         self._content = Audio.HOST_PREFIX+"storage/audio"+self._filename
 
