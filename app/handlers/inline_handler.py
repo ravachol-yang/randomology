7eb747f11af774bf2d3e2cceb3c13f2d41f77b17
Ravachol <ravachol.yang@gmail.com>
2024-03-28 19:46:15 +0800

    try to support inline

--- app/handlers/inline_handler.py
+++ app/handlers/inline_handler.py
@@ -4,16 +4,17 @@ from telebot import TeleBot
 from telebot import types
 
 from app.models.text import Text
+from app.models.voice import Voice
 
 def inline_dispatch(inline_query, bot: TeleBot, data:dict):
     options = data['options']
     text = Text()
+    voice = Voice()
     try:
         text = types.InlineQueryResultArticle('1', 'Random Text',
                                               types.InputTextMessageContent(text.generate(options).content()))
-        audio = types.InlineQueryResultArticle('2',
-                                               'TODO',
-                                               types.InputTextMessageContent("TODO"))
+        voice = types.InlineQueryResultVoice('2', 'Random Voice',
+                                             voice_url=voice.generate(options).to_voice().content())
         bot.answer_inline_query(inline_query.id, [text, audio], cache_time=0)
     except Exception as e:
         print(e)
